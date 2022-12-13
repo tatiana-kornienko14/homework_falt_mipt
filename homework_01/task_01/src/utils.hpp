05141b3fafa8372287dffbe53d0bd8d79d118fe2
Tatyana145 <kornienko.tiu@phystech.edu>
2022-12-14 00:55:40 +0300

    rm hw_01

--- homework_01/task_01/src/utils.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <string>
-#include <string_view>
-#include <vector>
-
-std::vector<std::string> SplitString(const std::string& data);
--- homework_01/task_02/src/utils.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <string>
-#include <string_view>
-#include <vector>
-
-int Calculate(const std::string& data);
