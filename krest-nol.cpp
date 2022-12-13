ea90e3cab3262c340e6c2a78533a66ba226829b0
Tatyana145 <kornienko.tiu@phystech.edu>
2022-12-14 00:47:50 +0300

    кн

---
+++ krest-nol.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+enum class Fled {
+  Free, Nought, Cross
+};
+
+enum class GameState {
+  Running, Draw, NoughtsWin, CrossesWin
+};
+
+class NandCGrid {
+  Field grid[3][3];
+public:
+  Field Get(size_t x, size_t y)const {
+    return grid[x][y];
+  }
+
+  void Set(size_t x, size_t y, Field f) {
+    grid[x][y] = f;
+  }
+
+  GameState GetState()const {};
+  std::string ToString()const {};
+};
+
+class NandCGame {
+  NandCGrid m_grid;
+  Player& m_player1;
+  Player& m_player2;
+public:
+  NandCGame(Player& pl1, Player& pl2);
+  void Run() {}
+};
+
+class Player {
+public:
+  virtual std::pair<size_t, size_t>TakeTurn(const NandCGrid& grid) {}
+};
+
+class RealPlayer:public Player {
+  //pass
+};
+
+class AIPlayer:public Player {
+  //pass
+};
